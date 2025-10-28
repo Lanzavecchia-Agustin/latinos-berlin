ae6dc9b72167429abd2149ef9b28f895f8e77735
Lanzavecchia-Agustin <lanzavecchia.job@gmail.com>
2025-10-28 19:19:29 -0300

    fix

--- next.config.ts
+++ next.config.ts
@@ -1,21 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  experimental: {
-    serverComponentsExternalPackages: ['@prisma/client', 'prisma']
-  },
-  reactStrictMode: true,
-  webpack: (config, { isServer }) => {
-    if (isServer) {
-      config.externals.push('@prisma/client')
-    }
-    if (!isServer) {
-      config.resolve.fallback = {
-        fs: false,
-      };
-    }
-    return config;
-  },
+  /* config options here */
 };
 
 export default nextConfig;
