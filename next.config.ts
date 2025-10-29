37fb957092351aeb15029ca6a11d4da841e4c9c2
Lanzavecchia-Agustin <lanzavecchia.job@gmail.com>
2025-10-29 18:23:52 -0300

    fix

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,17 @@
-import type { NextConfig } from "next";
+import { PrismaPlugin } from "@prisma/nextjs-monorepo-workaround-plugin";
 
-const nextConfig: NextConfig = {
-  /* config options here */
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+    devIndicators: false,
+    experimental: {
+        authInterrupts: true,
+    },
+    webpack: (config: { plugins: unknown[]; }, { isServer }: { isServer: boolean }) => {
+        if (isServer) {
+            config.plugins = [...config.plugins, new PrismaPlugin()];
+        }
+        return config;
+    },
 };
 
 export default nextConfig;
